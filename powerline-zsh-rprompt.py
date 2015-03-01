7e1d8b5936220c65ebb99a69ea083b04c66c6ad9
Ratheesh S <ratheeshreddy@gmail.com>
2015-03-01 18:18:07 +0530

    Change virtualenv background color
    Signed-off-by: Ratheesh S <ratheeshreddy@gmail.com>
    Tested-by: Ratheesh S <ratheeshreddy@gmail.com>

--- powerline-zsh-rprompt.py
+++ powerline-zsh-rprompt.py
@@ -26,7 +26,7 @@ class Color:
     CWD_FG = 11
     SEPARATOR_FG = 250
 
-    REPO_CLEAN_BG = 9 #180 #236
+    REPO_CLEAN_BG = 147 #180 #236
     REPO_CLEAN_FG = 16
     REPO_DIRTY_BG = 161  # blue
     REPO_DIRTY_FG = 15   # white
@@ -42,7 +42,7 @@ class Color:
     SVN_CHANGES_BG = 148
     SVN_CHANGES_FG = 22  # dark green
 
-    VIRTUAL_ENV_BG = 12 #35  # a mid-tone green
+    VIRTUAL_ENV_BG = 5 #35  # a mid-tone green
     VIRTUAL_ENV_FG = 15
 
 
